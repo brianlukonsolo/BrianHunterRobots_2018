ff1a139669d0a83bef804230399a39ab00084c0d
brianlukonsolo139 <brian.lukonsolo@gmail.com>
2018-03-18 16:14:03 +0000

    Refactored Properties.lua, main.lua and Behaviours.lua
    - Added some new properties to Properties.lua
    - Extracted some function input values to properties file

--- main.lua
+++ main.lua
@@ -3,49 +3,51 @@
 -- main.lua -> the most important file that runs the whole app
 --imports
 local robotFunctions = require('RobotFunctions.Behaviours')
-local properties = require('Configuration.Properties')
+local props = require('Configuration.Properties')
 --globals
 local killall = false
 -----------------------------------------------------------------------------------------
 
-local bg = display.newRect(0, 0, display.contentWidth, display.contentHeight)
+local bg = display.newRect(props.ZERO, props.ZERO, display.contentWidth, display.contentHeight)
 bg.x = display.contentWidth/2
 bg.y = display.contentHeight/2
-bg:setFillColor(0,0,0.2)
+bg:setFillColor(props.ZERO, props.ZERO, 0.2)
 
 --Robot clones factory
 local spawnTable = {}
 
 local function spawnHunterRobot()
-	local hunterRobot = display.newCircle(properties.BOT_SIZE, properties.BOT_SIZE, properties.BOT_SIZE)
-	hunterRobot.name = properties.DEFAULT_BOT_NAME
-	hunterRobot.x = math.random(properties.MAX_DISTANCE_LEFT, properties.MAX_DISTANCE_RIGHT)
-	hunterRobot.y = math.random(properties.MAX_DISTANCE_UP, properties.MAX_DISTANCE_DOWN)
-	hunterRobot.nextDestinationX = math.random(-10,10) + math.random(-10,10)
-	hunterRobot.nextDestinationY = math.random(-10,10) +math.random(-10,10)
-	hunterRobot:setFillColor(0.8,0,0)
+	local hunterRobot = display.newCircle(props.BOT_SIZE, props.BOT_SIZE, props.BOT_SIZE)
+	hunterRobot.name = props.DEFAULT_BOT_NAME
+	hunterRobot.bias = math.random(props.BIAS_MIN, props.BIAS_MAX)
+	hunterRobot.x = math.random(props.MAX_DISTANCE_LEFT, props.MAX_DISTANCE_RIGHT)
+	hunterRobot.y = math.random(props.MAX_DISTANCE_UP, props.MAX_DISTANCE_DOWN)
+	hunterRobot.nextDestinationX = math.random(props.BOT_DESTINATION_MIN, props.BOT_DESTINATION_MAX)
+	hunterRobot.nextDestinationY = math.random(props.BOT_DESTINATION_MIN, props.BOT_DESTINATION_MAX)
+	hunterRobot:setFillColor(0.8, props.ZERO, props.ZERO)
 
 	return hunterRobot
 
 end
 
 --Spawning the bots
-for i=1, properties.NUMBER_OF_BOTS_TO_SPAWN do
+for i=1, props.NUMBER_OF_BOTS_TO_SPAWN do
  spawnTable[i] = spawnHunterRobot()
- spawnTable[i].name = 'Bot' .. tostring(math.random(0, properties.MAX_BOT_POPULATION))
+ spawnTable[i].name = 'Bot' .. tostring(math.random(props.ZERO, props.MAX_BOT_POPULATION))
 end
 
 function allBotsExplore()
 	for c=1, #spawnTable do
 		robotFunctions.pickNewCoordinates(spawnTable[c])
-		timer.performWithDelay(math.random(10,5000), robotFunctions.move(spawnTable[c]))
+		timer.performWithDelay(math.random(props.SINGLE_BOT_TIMER_MIN, 
+		props.SINGLE_BOT_TIMER_MAX), robotFunctions.move(spawnTable[c]))
 	end
 
 end
 
 --bots explore forever with interval
 local timedEvent = timer.performWithDelay(
-	math.random(properties.TIMER_MINIMUM, properties.TIMER_MAXIMUM), allBotsExplore, 0)
+	math.random(props.TIMER_MINIMUM, props.TIMER_MAXIMUM), allBotsExplore, props.ZERO)
 
 
 --End of program-------------------------------------------------------------------------
